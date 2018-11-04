66e66885b9470c489bef2db602de01c4bfb03ce1
RayHaha <g820324@gmail.com>
2018-11-03 17:09:11 -0700

    add the ability of passing data

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ function Greetr(){
 util.inherits(Greetr, EventEmitter);
 
 // set the own function of Greetr
-Greetr.prototype.greet = function(){
-	console.log(this.greeting);
-	this.emit('greet');
+Greetr.prototype.greet = function(data){
+	console.log(this.greeting + ': ' + data);
+	this.emit('greet', data);
 }
 
 // create new object
 var greeter1 = new Greetr();
 
 // greeter1 has accessed to the methods and properties of both Greetr and EventEmitter
-greeter1.on('greet', function(){
-	console.log('Someone greeted!');
+greeter1.on('greet', function(data){
+	console.log('Someone greeted!: ' + data);
 });
 
-greeter1.greet();
+greeter1.greet('Tony');
