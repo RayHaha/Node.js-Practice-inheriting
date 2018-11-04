a23fecbe8de10d64c06631765bf91c633b3fc174
RayHaha <g820324@gmail.com>
2018-11-03 17:44:21 -0700

    use class to build new object

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 var EventEmitter = require('events');
-var util = require('util');
 
+// use class to build new object
+// extends means that Greetr inherits from EventEmitter
+class Greetr extends EventEmitter{
+	constructor(){
+		super();	// it did the same as EventEmitter.call(this);
+		this.greeting = 'Hello World!';
+	}
+
+	greet(data){
+		console.log(this.greeting + ': ' + data);
+		this.emit('greet', data);
+	}
+}
+
+/*
 function Greetr(){
+	// we use var greeter1 = new Greetr(); to create a new object
+	// so the "this" here is an new object
+	// we need to use .call to pass the new object that has already been created
+	// inside the EventEmitter function constructor, it's passed by reference
+	// it's also called super constructor
+	EventEmitter.call(this);
 	this.greeting = 'Hello World!';
 }
 
@@ -13,6 +33,7 @@ Greetr.prototype.greet = function(data){
 	console.log(this.greeting + ': ' + data);
 	this.emit('greet', data);
 }
+*/
 
 // create new object
 var greeter1 = new Greetr();
